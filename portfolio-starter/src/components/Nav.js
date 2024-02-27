4474df430fdbd21c6ac92ffc03bd6d1ae459da57
ShaunBinoy <shaunpbinoyoff@gmail.com>
2024-02-27 12:03:04 +0530

    completed a structure and started working on nav bar

---
+++ portfolio-starter/src/components/Nav.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+//Importing Icons
+import { BiHomeAlt, BiUser } from "react-icons/bi";
+import { BsClipboard2Data, BsBriefcase, BsChatSquare } from "react-icons/bs";
+
+//Link
+import { Link } from "react-scroll";
+
+const Nav = () => {
+  return <nav>
+    <div className="container mx-auto">
+      
+    </div>
+  </nav>;
+};
+
+export default Nav;
