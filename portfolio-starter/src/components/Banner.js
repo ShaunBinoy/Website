a90cb8e6d87731b04888e8fe490affa06c3799e4
ShaunBinoy <shaunpbinoyoff@gmail.com>
2024-02-28 13:09:57 +0530

    COMPLETED THE FIRST PAGE

--- portfolio-starter/src/components/Banner.js
+++ portfolio-starter/src/components/Banner.js
@@ -1,19 +1,93 @@
 import React from "react";
-import Image from "../assets/avatar.png";
-import { FaGithub, FaYoutube, FaDribble } from "react-icons/fa";
+import Image from "../assets/avatar2.png";
+import { FaGithub, FaYoutube, FaDribbble } from "react-icons/fa";
 import { TypeAnimation } from "react-type-animation";
 import { motion } from "framer-motion";
 import { fadeIn } from "../variants";
 
 const Banner = () => {
   return (
-    <section className="section" id="home">
+    <section
+      className="min-h-[85vh] lg:min-h-[78vh] flex items-center"
+      id="home"
+    >
       <div className="container mx-auto">
-        <div>
-          <div>text</div>
-          <div>
-            <img src={Image} />
+        <div className="flex flex-col gap-y-8 lg:flex-row lg:items-center lg:gap-x-8">
+          <div className="flex-1 text-center font-secondary lg:text-left">
+            <motion.h1
+              variants={fadeIn("up", 0.3)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.7 }}
+              className="text-[35px] font-bold leading-[0.8] lg:text-[110px]"
+            >
+              SHAUN <span>P BINOY</span>
+            </motion.h1>
+            <motion.div
+              variants={fadeIn("up", 0.4)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.7 }}
+              className="mb-6 text-[36px] lg:text-[60px] font-secondary font-semibold uppercase leading-[1]"
+            >
+              <span className="">I am a </span>
+              <TypeAnimation
+                sequence={[
+                  "Developer...",
+                  2000,
+                  "Designer...",
+                  2000,
+                  "Photographer...",
+                  2000,
+                ]}
+                speed={50}
+                className="text-accent"
+                wrapper="span"
+                repeat={Infinity}
+              />
+            </motion.div>
+            <motion.p
+              variants={fadeIn("up", 0.5)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.7 }}
+              className="mx-w-lg mx-auto lg:mx-0"
+            >
+              a9ufsn9aunsc9iunqw9undfia ecianwiundfaiwdciqa aqnwdum qiwjxdqudnq
+              cqiubfni jcin 8fc8un8 8qwhf8 8fqh fu q8g wefwg8y
+            </motion.p>
+            <motion.div
+              variants={fadeIn("up", 0.6)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.7 }}
+              className="max-w-max mb-8 mx-auto lg:mx-0 mt-6 flex items-center gap-x-6"
+            >
+              <button className="btn btn-lg">Contact Me</button>
+              <div
+                className="flex text-[20px] gap-x-6 max-w-max mx-auto lg:mx-0"
+              >
+                <a href="#">
+                  <FaYoutube />
+                </a>
+                <a href="#">
+                  <FaDribbble />
+                </a>
+                <a href="#">
+                  <FaGithub />
+                </a>
+              </div>
+            </motion.div>
           </div>
+          <motion.div
+            variants={fadeIn("down", 0.3)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.7 }}
+            className="hidden lg:flex flex-1 max-w-[320px] lg:max-w-[482px] mx-auto"
+          >
+            <img src={Image} />
+          </motion.div>
         </div>
       </div>
     </section>
