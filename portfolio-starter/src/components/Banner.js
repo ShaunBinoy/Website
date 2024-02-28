a092105f170293c9944e585b367a24d02d2afe28
ShaunBinoy <shaunpbinoyoff@gmail.com>
2024-02-28 11:08:34 +0530

    COMPLETED THE BANNER SECTION

--- portfolio-starter/src/components/Banner.js
+++ portfolio-starter/src/components/Banner.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React from "react";
+import Image from "../assets/avatar.png";
+import { FaGithub, FaYoutube, FaDribble } from "react-icons/fa";
+import { TypeAnimation } from "react-type-animation";
+import { motion } from "framer-motion";
+import { fadeIn } from "../variants";
 
 const Banner = () => {
-  return <div>Banner</div>;
+  return (
+    <section className="section" id="home">
+      <div className="container mx-auto">
+        <div>
+          <div>text</div>
+          <div>
+            <img src={Image} />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Banner;
--- portfolio-starter/src/components/Header.js
+++ portfolio-starter/src/components/Header.js
@@ -1,7 +1,16 @@
 import React from 'react';
 
 const Header = () => {
-  return <div>Header</div>;
+  return <header className='py-8'>
+    <div className='container mx-auto'>
+      <div className='flex justify-between items-center'>
+        <a href='#'>
+          <h2>MY PORTFOLIO</h2>
+        </a>
+        <button className='btn btn-sm'>Work with me</button>
+      </div>
+    </div>
+  </header>;
 };
 
 export default Header;
